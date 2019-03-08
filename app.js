8337a058f40c3d769ac16b6714f2adc77c833949
hero0nezero <caseyharvey1@gmail.com>
2019-03-08 21:17:38 +1100

    added new photo gallery, updated links & tags

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+
 function sizer () {
-    cover = document.getElementById('cover').style;
-    height = document.getElementById('main').scrollHeight + 200;
-    cover.height = height + 'px';
+   cover = document.getElementById('cover').style;
+   height = main.scrollHeight + 200;
+   cover.height = height + 'px';
+    
 }
 sizer();
 window.onresize = sizer;
---
+++ app2.js
@@ -0,0 +1,43 @@
+// for port.html 
+
+// number of images in the gallery
+var listnum = 7;
+// number of images in the gallery
+
+// variables for DOM Elements
+var cover2 = document.getElementById('cov');
+var left = document.getElementById("left");
+var right = document.getElementById("right");
+// variables for DOM Elements
+
+// variable for the img count
+var img = 1;
+// variable for the img count
+
+// addEventListeners
+left.addEventListener('click', function () {
+
+    if (img === 1) {
+        cover2.style.backgroundImage = "url('img2/" + listnum + ".jpeg')";
+        img = listnum;
+
+    } else {
+        img--;
+        cover2.style.backgroundImage = "url('img2/" + img + ".jpeg')";
+
+    }
+
+});
+right.addEventListener('click', function () {
+
+    if (img === listnum) {
+        cover2.style.backgroundImage = "url('img2/1.jpeg')";
+        img = 1;
+
+    } else {
+        img++;
+        cover2.style.backgroundImage = "url('img2/" + img + ".jpeg')";
+
+    }
+
+});
