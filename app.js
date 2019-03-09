2c035af9caa1955639c50a2a87d31d842ce3a422
hero0nezero <caseyharvey1@gmail.com>
2019-03-09 20:55:46 +1100

    added new sub heading. stared work on new expand featur in projects

--- app.js
+++ app.js
@@ -9,4 +9,28 @@ function sizer () {
 setTimeout(sizer, 100);
 window.onresize = sizer;
 
+// var p1 = document.getElementById('p1');
+// var p2 = document.getElementById('p2');
+// var p3 = document.getElementById('p3');
+// var c1 = document.getElementsByClassName('c1');
+// var c2 = document.getElementsByClassName('c2');
+// var c3 = document.getElementsByClassName('c3');
 
+
+
+// p1.addEventListener('click', function() {
+//    c1.display = 'inline';
+//    setTimeout(sizer, 100);
+//    console.log(c1);
+// });
+// p2.addEventListener('click', function() {
+//    c2.display = 'inline';
+//    // setTimeout(sizer, 100);
+//    console.log(c2);
+// });
+// // p3.addEventListener('click', function() {
+// //    c3.style.display = 'inline';
+// //    setTimeout(sizer, 100);
+// // });
+// console.log(c1);
+// console.log(c2);
