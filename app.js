8af4333afad92245d7a12e09fff4610810bfb9f6
hero0nezero <caseyharvey1@gmail.com>
2019-03-09 18:35:48 +1100

    changed the styling of contact.html. some other small style changes

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ function sizer () {
    var cover = document.getElementById('cover').style;
    var main = document.getElementById('main').clientHeight + 300;
    cover.height = main + 'px';
-    console.log(main);
-    console.log(document.getElementById('main').offsetHeight + 200);
+    
 }
 // sizer();
 setTimeout(sizer, 100);
 window.onresize = sizer;
+
+
