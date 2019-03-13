165a78820e0517b2708c028a89bd178817a68a0d
hero0nezero <caseyharvey1@gmail.com>
2019-03-13 15:22:16 +1100

    worked on animations and onresize. works ocross devices now. lots of tiny little changes realted to the animation timing curve delay and style. webpage is live now

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-
+var cover = document.getElementById('cover');
+var mainw = document.getElementById('main');
+var cw ;
 function sizer () {
-   var cover = document.getElementById('cover').style;
-   var main = document.getElementById('main').clientHeight + 120;
-   cover.height = main + 'px';
-    
-}
-// sizer();
-setTimeout(sizer, 100);
-window.onresize = sizer;
+  
+   if (cw !== mainw.clientWidth) {
 
+      cover.classList.remove('aniback');
+      var main =  document.getElementById('main').scrollHeight +100;
+      cover.style.height = main + 'px';
+      cover.classList.add('aniback');
+      cw = mainw.clientWidth;
+      
+   }
+   
+}
+
+// sizer();
+setTimeout(sizer,200);
+window.onresize = sizer;
